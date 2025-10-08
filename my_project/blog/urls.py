b98df64f90bac3c49d3de10348f5d884c5419ce2
Bowie <134542537+araujo-angel@users.noreply.github.com>
2025-10-08 11:09:57 -0300

    terceira atividade rad

--- my_project/blog/urls.py
+++ my_project/blog/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path("hello/", views.hello),
     path("eco/<str:textoUrl>", views.eco),
-    path("info/", views.info)
+    path("info/", views.info),
+    path('nome/', views.nome, name='nome'),
+    path('condicionais/', views.condicionais, name='condicionais'),
+    path('loops/', views.loops, name='loops'),
+    path('', views.home, name='home'),
+    path('contato/<int:telefone>/', views.contato, name='contato'),
+    path('about/', views.about, name='about'),
 ]
--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
@@ -14,3 +15,37 @@ def info(request):
      "semestre": "2025.2"
     }
     return JsonResponse(informacoes)
+
+def home(request):
+    return render(request, "blog/home.html") #blog/ indica que esta na pasta blog de template, como no slide
+
+def about(request):
+    return render(request, "blog/about.html")
+
+def contato(request, telefone):
+    return render(request, "blog/contato.html", {"telefone": telefone})
+
+def nome(request):
+    contexto = {
+        "usuario": "Alex",
+        "numero": 10,
+        "data": date.today(),
+    }
+    return render(request, "blog/nome.html", contexto)
+
+def condicionais(request):
+    contexto = {
+        "is_logged_in": True,
+        "idade": 20,
+        "role": "admin",
+    }
+    return render(request, "blog/condicionais.html", contexto) 
+
+def loops(request):
+    produtos = [
+        {"nome": "Notebook", "preco": 3500},
+        {"nome": "Mouse", "preco": 80},
+        {"nome": "Teclado", "preco": 150},
+        {"nome": "Monitor", "preco": 1200},
+    ]
+    return render(request, "blog/loops.html", {"produtos": produtos})
